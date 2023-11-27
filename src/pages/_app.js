fd08b37eea6f52027eebf45d03c6a7f9c8171af0
MelikeDemiralay <melikedemiralay3215@gmail.com>
2023-11-27 03:32:32 +0300

    added navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
   return <Component {...pageProps} />
 }
