e3964cef751cf33e0f934e6a9207819a57c5d575
MelikeDemiralay <melikedemiralay3215@gmail.com>
2023-11-27 17:53:33 +0300

    axios added

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const baseURL = "https://fakestoreapi.com";
+
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+export const fetchProducts = async () => {
+  const response = await axiosInstance.get("/products");
+  return response.data;
 }
+
